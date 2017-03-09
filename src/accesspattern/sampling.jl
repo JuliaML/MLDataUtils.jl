e28ea534e8e5fdcef3ad4c5b17b6572dac63bc59
Lyndon White <oxinabox@ucc.asn.au>
2017-03-09 13:27:25 +0000

    =Add undersampling

--- src/accesspattern/sampling.jl
+++ src/accesspattern/sampling.jl
@@ -44,46 +44,42 @@ function oversample(data::Tuple, obsdim, by_input, byfun, post_shuffle)
 end
 
 """
-oversample(data; obsdim = default_obsdim(data), by_input=1, byfun=identity, post_shuffle=true) =
+undersample(data; obsdim = default_obsdim(data), by_input=1, byfun=identity, keep_order=false)
 Generate new data, by repeating existing elements so that their is the same number of every class.
 
  - `by_input` specifies the index for wich input will be used to take classes from
  - `byfun` specifies a function used to tranform the input into a class ID
- - `post_shuffle` determines if data will be shuffled after the operation; if it is not shuffled then all the repeated samples will be together at the end, sorted by class
+ - `keep_order` determines if data will be retained in the same order it was presented. If false then all samples will be sorted by class
 
 """
-function undersample(data; obsdim = default_obsdim(data), by_input=1, byfun=identity, post_shuffle=true)
-    undersample(data, obs_dim(obsdim), by_input, byfun, post_shuffle)
+function undersample(data; obsdim = default_obsdim(data), by_input=1, byfun=identity, keep_order=false)
+    undersample(data, obs_dim(obsdim), by_input, byfun, keep_order)
 end
 
-function undersample(data, obsdim, by_input, byfun, post_shuffle)
+function undersample(data, obsdim, by_input, byfun, keep_order)
     #make it all tuple of datas if it wasn't already
-    undersample((data,), (obsdim,), by_input, byfun, post_shuffle)
+    first(undersample((data,), (obsdim,), by_input, byfun, keep_order))
 end
 
-function undersample(data::Tuple, obsdim, by_input, byfun, post_shuffle)
-    inds = collect(1:nobs(data, obsdim)) #firstly we will start by keeping everything
+function undersample(data::Tuple, obsdim, by_input, byfun, keep_order)
+    inds = Int[]
 
     classes = DefaultDict(()->Int[])
     for ii in 1:nobs(data, obsdim)
-        key = byfun(getobs(data, ii)[by_input])
+        key = byfun(getobs(data, ii, obsdim)[by_input])
         push!(classes[key], ii)
     end
 
-    maxcount = maximum(length, values(classes))
-    sizehint!(inds, length(classes)*maxcount)
+    mincount = minimum(length, values(classes))
+    sizehint!(inds, length(classes)*mincount)
 
     for (class, class_inds) in classes
-        num_extra_needed = maxcount - length(class_inds)
-        while(num_extra_needed) > length(class_inds)
-            num_extra_needed-=length(class_inds)
-            push!(inds, class_inds)
-        end
-        push!(inds, sample(class_inds, num_extra_needed; replace=false))
+        append!(inds, sample(class_inds, mincount; replace=false))
+        #MICROOPT: Could do ordered=keep_order. However, what is the point since will still be class ordered? (This may sort faster, or may not; and may be slower to generate or may not)
     end
 
-    if post_shuffle
-        shuffle!(inds) #rather than using shuffleobs, cut out the middleman
+    if keep_order
+        sort!(inds)
     end
     datasubset(data, inds, obsdim)
 end
--- test/tst_sampling.jl
+++ test/tst_sampling.jl
@@ -11,6 +11,7 @@ srand(1)
         oversampled = oversample(src)
         @test all(counts(oversampled).==counts(oversampled)[1])
         @test all( x ∈ oversampled for x in unique(src))
+        @test length(oversampled) > n_src
     end
 
 
@@ -25,5 +26,33 @@ srand(1)
         @test all(counts(lbls_os).==counts(lbls_os)[1])
         @test all( x ∈ lbls_os for x in unique(lbls_os))
         @test nobs(data_os, MLDataUtils.ObsDim.First()) == nobs(lbls_os)
+        @test nobs(lbls_os) > n_src
+    end
+end
+
+
+@testset "Undersample" begin
+    @testset "Basic" begin
+        n_src = 2000
+        src = rand([1,2,2,3,3,3, 4,4,4,4], 2000)
+        sampled = undersample(src)
+        @test all(counts(sampled).==counts(sampled)[1])
+        @test all( x ∈ sampled for x in unique(src))
+        @test length(sampled) < n_src
+    end
+
+
+    @testset "Advanced" begin
+        n_src = 200
+        lbs = rand([1,2,2,3,3,3, 4,4,4,4], n_src)
+        data = rand(n_src, 50) #50 features
+
+        od = MLDataUtils.ObsDim.First(), MLDataUtils.ObsDim.First()
+
+        lbls_os, data_os = undersample((lbs, data); obsdim=od)
+        @test all(counts(lbls_os).==counts(lbls_os)[1])
+        @test all( x ∈ lbls_os for x in unique(lbls_os))
+        @test nobs(data_os, MLDataUtils.ObsDim.First()) == nobs(lbls_os)
+        @test nobs(lbls_os) < n_src
     end
 end
