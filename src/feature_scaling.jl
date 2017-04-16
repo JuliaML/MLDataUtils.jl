4cd7a1ce45615ae91347a00917849b8c59abbf0a
Andre Bieler <abieler@users.noreply.github.com>
2017-04-16 22:08:32 +0200

    rescale! and center! along obsdim (#30)

--- src/feature_scaling.jl
+++ src/feature_scaling.jl
@@ -1,53 +1,142 @@
 """
-`μ = center!(X[, μ])`
+`μ = center!(X[, μ, obsdim])`
 
-Centers each row of `X` around the corresponding entry in the vector `μ`.
+Centers `X` along obsdim around the corresponding entry in the vector `μ`.
 If `μ` is not specified then it defaults to `mean(X, 2)`.
 """
-function center!{T<:AbstractFloat}(X::AbstractMatrix{T}, μ::AbstractVector = vec(mean(X, 2)))
-    k, n = size(X)
-    @inbounds for j = 1:k
-        for i = 1:n
-            X[j, i] = X[j, i] - μ[j]
+function center!(X, μ; obsdim=LearnBase.default_obsdim(X))
+    center!(X, μ, convert(ObsDimension, obsdim))
+end
+
+function center!(X; obsdim=LearnBase.default_obsdim(X))
+    center!(X, convert(ObsDimension, obsdim))
+end
+
+function center!{T,N}(X::AbstractArray{T,N}, μ::AbstractVector, ::ObsDim.Last)
+    center!(X, μ, ObsDim.Constant{N}())
+end
+
+function center!{T,N}(X::AbstractArray{T,N}, ::ObsDim.Last)
+    center!(X, ObsDim.Constant{N}())
+end
+
+function center!{T,N,M}(X::AbstractArray{T,N}, obsdim::ObsDim.Constant{M})
+    μ = vec(mean(X, M))
+    center!(X, μ, obsdim)
+end
+
+function center!{T}(X::AbstractVector{T}, ::ObsDim.Constant{1})
+    μ = mean(X)
+    for i in 1:length(X)
+        X[i] = X[i] - μ
+    end
+    μ
+end
+
+function center!(X::AbstractVector, μ::AbstractVector, ::ObsDim.Constant{1})
+    @inbounds for i in 1:length(X)
+        X[i] = X[i] - μ[i]
+    end
+    μ
+end
+
+function center!(X::AbstractVector, μ::AbstractFloat, ::ObsDim.Constant{1})
+    @inbounds for i in 1:length(X)
+        X[i] = X[i] - μ
+    end
+    μ
+end
+
+function center!(X::AbstractMatrix, μ::AbstractVector, ::ObsDim.Constant{1})
+    nObs, nVars = size(X)
+    for iVar in 1:nVars
+        @inbounds for iObs in 1:nObs
+            X[iObs, iVar] = X[iObs, iVar] - μ[iVar]
         end
     end
     μ
 end
 
-function center!{T<:AbstractFloat}(x::AbstractVector{T}, μ::Real = mean(x))
-    for i = 1:length(x)
-        x[i] -= μ
+function center!(X::AbstractMatrix, μ::AbstractVector, ::ObsDim.Constant{2})
+    nVars, nObs = size(X)
+    for iObs in 1:nObs
+        @inbounds for iVar in 1:nVars
+            X[iVar, iObs] = X[iVar, iObs] - μ[iVar]
+        end
     end
     μ
 end
 
-"""
-`μ, σ = rescale!(X[, μ, σ])`
 
-Centers each row of `X` around the corresponding entry in the vector `μ`
-and then rescaled using the corresponding entry in the vector `σ`.
-If `μ` is not specified then it defaults to `mean(X, 2)`.
-If `σ` is not specified then it defaults to `std(X, 2)`.
 """
-function rescale!{T<:AbstractFloat}(X::AbstractMatrix{T}, μ::AbstractVector = vec(mean(X, 2)), σ::AbstractVector = vec(std(X, 2)))
-    k, n = size(X)
-    @inbounds for j = 1:k
-        for i = 1:n
-            X[j, i] = X[j, i] - μ[j]
-            if σ[j] > 0
-                X[j, i] = X[j, i] / σ[j]
-            end
+`μ, σ = rescale!(X[, μ, σ, obsdim])`
+
+Centers `X` along obsdim around the corresponding entry in the vector `μ`
+and then rescaled using the corresponding entry in the vector `σ`.
+"""
+function rescale!(X, μ, σ; obsdim=LearnBase.default_obsdim(X))
+    rescale!(X, μ, σ, convert(ObsDimension, obsdim))
+end
+
+function rescale!{T,N}(X::AbstractArray{T,N}, μ, σ, ::ObsDim.Last)
+    rescale!(X, μ, σ, ObsDim.Constant{N}())
+end
+
+function rescale!(X; obsdim=LearnBase.default_obsdim(X))
+    rescale!(X, convert(ObsDimension, obsdim))
+end
+
+function rescale!{T,N}(X::AbstractArray{T,N}, ::ObsDim.Last)
+    rescale!(X, ObsDim.Constant{N}())
+end
+
+function rescale!{T,N,M}(X::AbstractArray{T,N}, obsdim::ObsDim.Constant{M})
+    μ = vec(mean(X, M))
+    σ = vec(std(X, M))
+    rescale!(X, μ, σ, obsdim)
+end
+
+function rescale!(X::AbstractVector, ::ObsDim.Constant{1})
+    μ = mean(X)
+    σ = std(X)
+    for i in 1:length(X)
+        X[i] = (X[i] - μ) / σ
+    end
+    μ, σ
+end
+
+function rescale!(X::AbstractMatrix, μ::AbstractVector, σ::AbstractVector, ::ObsDim.Constant{2})
+    σ[σ .== 0] = 1
+    nVars, nObs = size(X)
+    for iObs in 1:nObs
+        @inbounds for iVar in 1:nVars
+            X[iVar, iObs] = (X[iVar, iObs] - μ[iVar]) / σ[iVar]
         end
     end
     μ, σ
 end
 
-function rescale!{T<:AbstractFloat}(x::AbstractVector{T}, μ::Real = mean(x), σ::Real = std(x))
-    for i = 1:length(x)
-        x[i] -= μ
-        if σ > 0
-            x[i] /= σ
+function rescale!(X::AbstractMatrix, μ::AbstractVector, σ::AbstractVector, ::ObsDim.Constant{1})
+    σ[σ .== 0] = 1
+    nObs, nVars = size(X)
+    for iVar in 1:nVars
+        @inbounds for iObs in 1:nObs
+            X[iObs, iVar] = (X[iObs, iVar] - μ[iVar]) / σ[iVar]
         end
+    end
+    μ, σ
+end
+
+function rescale!(X::AbstractVector, μ::AbstractVector, σ::AbstractVector, ::ObsDim.Constant{1})
+    @inbounds for i in 1:length(X)
+        X[i] = (X[i] - μ[i]) / σ[i]
+    end
+    μ, σ
+end
+
+function rescale!(X::AbstractVector, μ::AbstractFloat, σ::AbstractFloat, ::ObsDim.Constant{1})
+    @inbounds for i in 1:length(X)
+        X[i] = (X[i] - μ) / σ
     end
     μ, σ
 end
@@ -85,5 +174,3 @@ function StatsBase.predict{T<:Real}(cs::FeatureNormalizer, X::AbstractMatrix{T})
     X = convert(AbstractMatrix{AbstractFloat}, X)
     StatsBase.predict!(cs, X)
 end
-
-
